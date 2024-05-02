90ffbf6cf706320dea58d71b68147d63cf97bbbc
Brendan Leighton <br3ndan.l8n@gmail.com>
2024-05-02 12:59:25 -0400

    updated test data

--- src/data/TestData.js
+++ src/data/TestData.js
@@ -5,21 +5,21 @@ const TestData = [
 		new Expression('exp 0', [
 			new Operand('1', 'num 1'),
 			new Operator('+'),
-			new Operand('1', 'num 2')
+			new Operand('2', 'num 2')
 		])
 	]),
 	new IEquation([
 		new Expression('exp 1', [
 			new Operand('1', 'num 1'),
-			new Operator('+'),
-			new Operand('1', 'num 2')
-		]),
+			new Operator('-'),
+			new Operand('2', 'num 2')
+		], true),
 		new Operator('+'),
 		new Expression('exp 2', [
-			new Operand('1', 'num 3'),
-			new Operator('+'),
-			new Operand('1', 'num 4')
-		])
+			new Operand('3', 'num 3'),
+			new Operator('*'),
+			new Operand('3', 'num 4')
+		], true)
 	])
 ]
 
